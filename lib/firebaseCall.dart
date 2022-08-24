68b5210147594362272fc8c1a08ab1507e1b8fe1
Amit <aramola20@gmail.com>
2022-08-25 02:20:39 +0530

    Firebase ui working

--- lib/firebaseCall.dart
+++ lib/firebaseCall.dart
@@ -9,16 +9,53 @@ class FirebaseCall extends StatefulWidget {
   State<FirebaseCall> createState() => _FirebaseCallState();
 }
 
+final fc = FirebaseFirestore.instance.collection('Employee');
+
 class _FirebaseCallState extends State<FirebaseCall> {
   getData() async {
-    await FirebaseFirestore.instance.collection('Employee');
+    await fc.get().then((value) {
+      if (value.docs.isNotEmpty) {
+        print(value.docs[0]['id']['name']['salary']['D.O.B']);
+      }
+    });
+  }
+
+  setData() async {
+    await fc.add({});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(),
-    );
+        appBar: AppBar(),
+        body: SizedBox(
+          child: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('Employee').snapshots(),
+            builder: (_, AsyncSnapshot<QuerySnapshot> snap) {
+              print(snap.data!.docs[0]['id']['name']);
+              return DataTable(columns: [
+                DataColumn(
+                  label: Text('Id'),
+                ),
+                DataColumn(
+                  label: Text('Name'),
+                ),
+                DataColumn(
+                  label: Text('Date of Birth'),
+                ),
+                DataColumn(
+                  label: Text('Salary'),
+                ),
+              ], rows: []);
+            },
+          ),
+        ));
   }
 }
