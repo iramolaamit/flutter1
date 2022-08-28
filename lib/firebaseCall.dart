60333bc3bcf4cba6586165a4dde511ac657d711f
Amit <aramola20@gmail.com>
2022-08-29 00:35:11 +0530

    firebase getdata and set data

--- lib/firebaseCall.dart
+++ lib/firebaseCall.dart
@@ -13,18 +13,24 @@ class FirebaseCall extends StatefulWidget {
 class _FirebaseCallState extends State<FirebaseCall> {
   final fc = FirebaseFirestore.instance.collection('Employee');
   List<dynamic>? fireList;
+  static int count = 2;
 
   getData() async {
     await fc.get().then((value) {
       if (value.docs.isNotEmpty) {
-        print(value.docs[0]['id']['name']['salary']['D.O.B']);
+        print(value.docs.first['id']);
+        print(value.docs.first['name']);
+        print(value.docs.first['department']);
+        print(value.docs.first['dob']);
       }
     });
   }
 
   setData() async {
-    await fc.add({});
+    count++;
+    await fc.add({'id': count, 'department': 'Devloper', 'name': 'Karan'});
   }
+
   //
   @override
   void initState() {
@@ -35,54 +41,71 @@ class _FirebaseCallState extends State<FirebaseCall> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            IconButton(onPressed: () {getData();
-              print('Data is fetched');}, icon: Icon(Icons.add)),
-            SizedBox(
-              height: 500,
-              width: double.infinity,
-              child: StreamBuilder(
-                stream: fc.snapshots(),
-                builder: (_, AsyncSnapshot<QuerySnapshot> snap) {
-                  print(snap.data!.docs[0]['name']);
-                  return ListView.builder(
-                    itemBuilder: (context, index) =>
-                        ListTile(title: Text(snap.data!.docs[index]['name']),
-                          // leading: Text(snap.data!.docs[index]['id']),
-                          // subtitle: Text(snap.data!.docs[index]['D.O.B']),
-                        ),
-                  );
-                  // ? LinearProgressIndicator()
-                  // : DataTable(
-                  //     columns: [
-                  //         DataColumn(
-                  //           label: Text('Id'),
-                  //         ),
-                  //         DataColumn(
-                  //           label: Text('Name'),
-                  //         ),
-                  //         DataColumn(
-                  //           label: Text('Date of Birth'),
-                  //         ),
-                  //         DataColumn(
-                  //           label: Text('Salary'),
-                  //         ),
-                  //       ],
-                  //     rows: fireList!
-                  //         .map((map) => DataRow(cells: [
-                  //               DataCell(Text(map['id'])),
-                  //               DataCell(Text(map['name'])),
-                  //               DataCell(Text(map['salary'])),
-                  //               DataCell(Text(map['D.O.B'])),
-                  //             ]))
-                  //         .toList());
-                },
-              ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                setData();
+              },
+              icon: Icon(Icons.update_rounded))
+        ],
+      ),
+      body: Column(
+        children: [
+          IconButton(
+              onPressed: () {
+                getData();
+                print('Data is fetched');
+              },
+              icon: Icon(Icons.add)),
+          SizedBox(
+            height: 500,
+            width: double.infinity,
+            child: StreamBuilder(
+              stream: fc.snapshots(),
+              builder: (_, AsyncSnapshot<QuerySnapshot> snap) {
+                // print(snap.data!.docs[0]['name']);
+                // print(snap.data!.docs[0]['dob'].toString());
+                // print(snap.data!.docs[0]['id'].toString());
+                // print(snap.data!.docs[0]['department']);
+                return ListView.builder(
+                  itemCount: snap.data!.size,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text(snap.data!.docs[index]['name']),
+                    trailing: Text(snap.data!.docs[index]['department']),
+                    leading: Text(snap.data!.docs[index]['id'].toString()),
+                    // subtitle: Text(snap.data!.docs[index]['dob'].toString()),
+                  ),
+                );
+                // ? LinearProgressIndicator()
+                // : DataTable(
+                //     columns: [
+                //         DataColumn(
+                //           label: Text('Id'),
+                //         ),
+                //         DataColumn(
+                //           label: Text('Name'),
+                //         ),
+                //         DataColumn(
+                //           label: Text('Date of Birth'),
+                //         ),
+                //         DataColumn(
+                //           label: Text('Salary'),
+                //         ),
+                //       ],
+                //     rows: fireList!
+                //         .map((map) => DataRow(cells: [
+                //               DataCell(Text(map['id'])),
+                //               DataCell(Text(map['name'])),
+                //               DataCell(Text(map['department'])),
+                //               DataCell(Text(map['dob'])),
+                //             ]))
+                //         .toList());
+              },
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
