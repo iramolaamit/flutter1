bc1852d704512621b2c3fc6a02eb62a494f6f4de
Amit <aramola20@gmail.com>
2022-08-29 00:34:37 +0530

    Api get

--- lib/apiDisplay.dart
+++ lib/apiDisplay.dart
@@ -11,6 +11,7 @@ class ApiCall extends StatefulWidget {
 
 class _ApiCallState extends State<ApiCall> {
   bool isTrue = false;
+  List<dynamic>? list;
   var apiData;
   @override
   Widget build(BuildContext context) {
@@ -23,9 +24,9 @@ class _ApiCallState extends State<ApiCall> {
               onPressed: () async {
                 isTrue = false;
                 setState(() {});
-                await ApiManger().getApiData().then((value) {
+                list = await ApiManger().getApiData().then((value) {
                   print(value);
-                  apiData =value;
+                  apiData = value;
                   isTrue = true;
                   setState(() {});
                 });
@@ -40,19 +41,30 @@ class _ApiCallState extends State<ApiCall> {
           ? Center(
               child: Text("Api is not called", textScaleFactor: 1.5),
             )
-          : ListView.builder(
+          :
+      // DataTable(
+      //         columns: [
+      //             DataColumn(label: Text('ID')),
+      //             DataColumn(label: Text('Name')),
+      //             DataColumn(label: Text('Date and time')),
+      //           ],
+      //         rows: list!
+      //             .map(
+      //               (map) => DataRow(cells: [
+      //                 DataCell(Text(map['id'].toString())),
+      //                 DataCell(Text(map['EmployeeId'].toString())),
+      //                 DataCell(Text(map['Phone Number'].toString())),
+      //               ] ),
+      //             )
+      //             .toList()),
+      ListView.builder(
+              itemCount: apiData.length,
               itemBuilder: (context, index) {
-                return
-                  ListTile(
-                    title: Text(apiData[index]),
-                    subtitle: Text(apiData[index]),
-                    leading: Text(apiData[index]),
+                return ListTile(
+                  title: Text(apiData[index]['EmployeeId'].toString()),
+                  subtitle: Text(apiData[index]['Phone Number'].toString()),
+                  leading: Text(apiData[index]['id'].toString()),
 
-
-                  // ListTile(
-                  // title: Text(apiData[index]['rwaName']),
-                  // subtitle: Text(apiData[index]['pinCode'].toString()),
-                  // leading: Text(apiData[index]['rwaId'].toString()),
 
                 );
               },
